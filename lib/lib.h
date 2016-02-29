6c3f40909f5898c7b1f4df5e854e02ceaba8bed5
lleaff <lleaff@users.noreply.github.com>
2016-02-29 21:52:51 +0100

    itoa

--- lib/lib.h
+++ lib/lib.h
@@ -13,14 +13,15 @@ void my_put_nbr(int n);
 char *my_readfile(char* filename);
 char *my_readline();
 char *my_readnchars(int n);
+char *my_itoa(int n);
 int  my_getnbr(char *str);
 int  is_int(char *str);
 int  str_eq(char* str1, char* str2);
 int  my_strncmp(char *s1, char *s2, int n);
 char *my_strstr(char *str, char *to_find);
 char **my_str_to_wordtab(char *str);
-void  my_strpush(char* dest, char* to_push);
-void  my_strnpush(char* dest, char* toPush, int n);
+void my_strpush(char* dest, char* to_push);
+void my_strnpush(char* dest, char* toPush, int n);
 
 #ifdef DEBUG
 #include "debug.h"
---
+++ lib/my_itoa.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+char *my_itoa(int n)
+{
+  int sign;
+  int l;
+  int m;
+  char *str;
+
+  sign = n >= 0 ? 1 : -1;
+  for (l = 0, m = n; (sign >= 0 && m > 0) || m < 0; m /= 10)
+    l++;
+  if (sign == -1)
+    l++;
+  l = MAX(1, l);
+  if ((str = malloc(sizeof(char) * (l + 1))) == NULL)
+    return (NULL);
+  str[l] = '\0';
+  if (sign == -1)
+    str[0] = '-';
+  else if (n == 0)
+    str[0] = '0';
+  for (; (sign >= 0 && n > 0) || n < 0; n /= 10, l--)
+    str[l - 1] = (n % 10) * sign + '0';
+  return (str);
+}
