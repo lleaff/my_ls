fa13286b21457cce4b19b609feda151c6e3a24d9
lleaff <lleaff@users.noreply.github.com>
2016-03-04 16:47:09 +0100

    reorg help

--- src/args.h
+++ src/args.h
@@ -6,6 +6,12 @@
 #define USAGE "USAGE: "BIN_NAME" [OPTION]... [FILE]...\n\n\
 OPTIONS:\n\
   -l\t Long list format.\n\
+  -m\t Stream output format. Display filenames on the same line separated by\
+ commas.\n\
+  -1\t Force output to be one entry per line. This is the default when output\
+is not a terminal.\n\
+  -C\t Force multi column output. This is the default when output is a\
+ terminal.\n\
   -R\t Recursive.\n\
   -r\t Reverse sort order.\n\
   -d\t Don't follow links given as arguments.\n\
@@ -18,12 +24,6 @@ OPTIONS:\n\
   -f\t Don't sort output. This option turns on the -a option.\n\
   -F\t Display file type symbols after filenames (/ for directories,\
  @ for symbolic links, etc...).\n\
-  -1\t Force output to be one entry per line. This is the default when output\
-is not a terminal.\n\
-  -C\t Force multi column output. This is the default when output is a\
- terminal.\n\
-  -C\t Stream output format. Display filenames on the same line separated by\
- commas.\n\
   -h\t Use human readable file sizes in long list format.\n\
   --help Print help\n\
 "
